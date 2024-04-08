0f789aaf6f3f980982267bbda48eb87d40b974a9
fcrozatier <frederic.crozatier@protonmail.com>
2024-04-08 14:43:01 +0200

    fix tailwind

--- src/routes/toggle/+page.svelte
+++ src/routes/toggle/+page.svelte
@@ -1,11 +1,11 @@
 <script>
 	import { createPressToggle } from '$lib/builders/toggle/press.svelte';
+	import Toggle from '$lib/ui/toggle/toggle.svelte';
 
 	let checked = $state(false);
-	let change = $state(false);
 
 	const toggle = createPressToggle({
-		pressed: true
+		pressed: false
 	});
 
 	$effect(() => {
@@ -13,17 +13,22 @@
 	});
 </script>
 
-<h1>statee to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<label>
-	<span>change</span>
-	<input type="checkbox" bind:checked={change} />
-</label>
 <label>
 	<span>disable</span>
 	<input type="checkbox" bind:checked />
 </label>
 
 <button use:toggle.action> I'm {toggle.state.pressed ? '' : 'not'} pressed</button>
-state
+
+<button
+	use:toggle.action
+	aria-label="Toggle italic"
+	class="grid h-9 w-9 place-items-center items-center justify-center rounded-md
+	bg-white text-base leading-4 text-magnum-800 shadow-lg hover:bg-magnum-100
+    disabled:cursor-not-allowed [aria-pressed='false']:bg-magnum-200
+    [aria-pressed='true']:text-magnum-900"
+>
+	I
+</button>
+
+<Toggle variant="outline">toggle</Toggle>
