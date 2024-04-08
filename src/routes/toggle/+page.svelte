e6a3727326a0ca1cbf6c4d632d010c8e08897075
fcrozatier <frederic.crozatier@protonmail.com>
2024-04-08 11:58:34 +0200

    toggle playground

---
+++ src/routes/toggle/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { createPressToggle } from '$lib/builders/toggle/press.svelte';
+
+	let checked = $state(false);
+	let change = $state(false);
+
+	const toggle = createPressToggle({
+		pressed: true
+	});
+
+	$effect(() => {
+		toggle.state.disabled = checked;
+	});
+</script>
+
+<h1>statee to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<label>
+	<span>change</span>
+	<input type="checkbox" bind:checked={change} />
+</label>
+<label>
+	<span>disable</span>
+	<input type="checkbox" bind:checked />
+</label>
+
+<button use:toggle.action> I'm {toggle.state.pressed ? '' : 'not'} pressed</button>
+state
