979d292d7ddde1bf6af73c9b263126b5f3195e6b
fcrozatier <frederic.crozatier@protonmail.com>
2024-04-08 15:26:45 +0200

    can disable switch

--- src/lib/builders/switch/switch.svelte.ts
+++ src/lib/builders/switch/switch.svelte.ts
@@ -1,6 +1,12 @@
+import { updateAttribute, updateBooleanAttribute } from "$lib/internal/helpers";
 import { key } from "$lib/utils/keyboard";
 
 import type { Action } from "svelte/action";
+
+export type CreateSwitch = {
+	checked?: boolean;
+	disabled?: boolean | undefined | null;
+};
 
 /**
  * Switch Toggle
@@ -13,35 +19,48 @@ import type { Action } from "svelte/action";
  * https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/Switch_role#all_descendants_are_presentational
  *
  */
-export const createSwitchToggle = (options?: { checked?: boolean }) => {
+export const createSwitchToggle = (options?: CreateSwitch) => {
 	let checked: boolean = $state(options?.checked ?? false);
+	let disabled: boolean = $state(options?.disabled ?? false);
 	let element: HTMLElement | undefined;
 
 	const handleClick = () => {
+		if (disabled) return;
 		checked = !checked;
-		updateAttributes();
 	};
 
 	const handleKeydown = (e: KeyboardEvent) => {
 		if (e.key === key.ENTER || e.key === key.SPACE) handleClick();
 	};
 
-	const updateAttributes = () => {
-		if (element instanceof HTMLInputElement) {
-			element?.setAttribute("checked", String(checked));
-		} else {
-			element?.setAttribute("aria-checked", String(checked));
-		}
-	};
+	const cleanup = $effect.root(() => {
+		$effect(() => {
+			if (element instanceof HTMLInputElement) {
+				updateAttribute(element, "checked", checked);
+			} else {
+				updateAttribute(element, "aria-checked", checked);
+			}
+			updateBooleanAttribute(element, "disabled", disabled);
+		});
+	});
 
 	return {
-		get checked() {
-			return checked;
-		},
+		state: {
+			get checked() {
+				return checked;
+			},
 
-		set checked(newVal) {
-			checked = newVal;
-			updateAttributes();
+			set checked(newVal) {
+				checked = newVal;
+			},
+
+			get disabled() {
+				return disabled;
+			},
+
+			set disabled(v) {
+				disabled = v;
+			},
 		},
 
 		action: ((node) => {
@@ -62,8 +81,11 @@ export const createSwitchToggle = (options?: { checked?: boolean }) => {
 				destroy() {
 					node.removeEventListener("click", handleClick);
 					node.removeEventListener("keydown", handleKeydown);
+					cleanup();
 				},
 			};
 		}) satisfies Action,
+
+		options,
 	};
 };
