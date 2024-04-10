5a533f91ba736f7275d72f1b05c2191c6fec5008
fcrozatier <frederic.crozatier@protonmail.com>
2024-04-10 13:53:52 +0200

    add tooltip

---
+++ src/lib/ui/tooltip/tooltip.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+	import { createTooltip, type CreateTooltip } from "$lib/builders/tooltip/tooltip.svelte";
+	import { cn } from "$lib/utils/styles";
+	import type { Snippet } from "svelte";
+	import type { HTMLAttributes } from "svelte/elements";
+
+	interface TooltipProps extends HTMLAttributes<HTMLDivElement>, CreateTooltip {
+		children: Snippet;
+	}
+
+	let { class: className, position, children }: TooltipProps = $props();
+
+	const tooltip = createTooltip({ position });
+</script>
+
+<div
+	class={cn(
+		"choco-tooltip bg-accent-foreground text-accent pointer-events-none absolute z-10 rounded py-1 px-2 text-center text-base opacity-0 shadow select-none",
+		className,
+	)}
+	use:tooltip.action
+>
+	{@render children()}
+</div>
+
+<style>
+	/* create a stacking context for elements with > .choco-tooltips */
+	:global(:has(> .choco-tooltip)) {
+		position: relative;
+	}
+
+	.choco-tooltip {
+		--margin: 6px;
+
+		transform: scale(0.8) translateX(var(--x, 0)) translateY(var(--y, 0));
+		transition:
+			opacity 200ms ease,
+			transform 100ms ease;
+
+		inline-size: max-content;
+		max-inline-size: 40ch;
+
+		&:global([data-position="top"]) {
+			bottom: calc(100% + var(--margin));
+			left: 50%;
+			--x: -50%;
+			transform-origin: center left;
+		}
+		&:global([data-position="bottom"]) {
+			top: calc(100% + var(--margin));
+			left: 50%;
+			--x: -50%;
+			transform-origin: center left;
+		}
+		&:global([data-position="left"]) {
+			right: calc(100% + var(--margin));
+			top: 50%;
+			--y: -50%;
+			transform-origin: top center;
+		}
+		&:global([data-position="right"]) {
+			left: calc(100% + var(--margin));
+			top: 50%;
+			--y: -50%;
+			transform-origin: top center;
+		}
+
+		&:global([data-open="true"]) {
+			opacity: 1;
+			transition-delay: 300ms;
+			transform: scale(1) translateX(var(--x, 0)) translateY(var(--y, 0));
+		}
+	}
+</style>
---
+++ src/routes/tooltip/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Tooltip from "$lib/ui/tooltip/tooltip.svelte";
+</script>
+
+<p>
+	Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi autem ipsum totam similique
+	tenetur corrupti officia, temporibus eaque quisquam consectetur unde assumenda placeat nulla alias
+	vel qui pariatur eos doloremque?
+</p>
+
+<button>button</button>
+
+<span class="">
+	HTML
+	<Tooltip position="right">Hyper Text Markup Language</Tooltip>
+</span>
