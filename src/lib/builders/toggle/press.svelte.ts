05d6209ce77a0f71d79a5d15973edbea249ada9b
fcrozatier <frederic.crozatier@protonmail.com>
2024-04-12 17:20:26 +0200

    fix toggle button

--- src/lib/builders/toggle/press.svelte.ts
+++ src/lib/builders/toggle/press.svelte.ts
@@ -12,9 +12,11 @@ export type CreateToggle = { pressed?: TriState; disabled?: boolean | undefined 
 const defaults = { pressed: false, disabled: false } satisfies CreateToggle;
 
 /**
- * Toggle button
+ * ## Toggle
  *
- * Semantics: pressed, not pressed, partially pressed ("mixed" state) or unset
+ * Use on button or input elements.
+ *
+ * Semantics: pressed, not pressed or partially pressed ("mixed" state)
  *
  * The label should not change when the state changes. Use `simpleToggle` if needed.
  */
@@ -22,21 +24,22 @@ export const createPressToggle = (options?: CreateToggle) => {
 	let state = $state({ ...defaults, ...options });
 	let element: HTMLElement | undefined = $state();
 
-	const handleClick = () => {
+	const handler = (e: MouseEvent | KeyboardEvent) => {
 		if (state.disabled) return;
-		state.pressed = !state.pressed;
-	};
-
-	const handleKeydown = (e: KeyboardEvent) => {
-		if (e.key === key.ENTER || e.key === key.SPACE) {
-			// Avoid firing a synthetic click event when pressing Space on button (over-firing)
-			e.preventDefault();
-			handleClick();
+		if (e instanceof KeyboardEvent) {
+			if (e.key !== key.ENTER && e.key !== key.SPACE) return;
+			// Prevent triggering the synthetic click event on input elements
+			else e.preventDefault();
 		}
+		state.pressed = !state.pressed;
 	};
 
 	const cleanup = $effect.root(() => {
 		$effect(() => {
+			if (element instanceof HTMLInputElement) {
+				updateBooleanAttribute(element, "checked", state.pressed === true);
+				updateBooleanAttribute(element, "indeterminate", state.pressed === "mixed");
+			}
 			updateAttribute(element, "aria-pressed", state.pressed);
 			updateBooleanAttribute(element, "disabled", state.disabled);
 		});
@@ -54,16 +57,22 @@ export const createPressToggle = (options?: CreateToggle) => {
 		action: ((node) => {
 			element = node;
 
+			if (node instanceof HTMLInputElement) {
+				node.setAttribute("type", "checkbox");
+				updateBooleanAttribute(element, "checked", state.pressed === true);
+				updateBooleanAttribute(element, "indeterminate", state.pressed === "mixed");
+			}
+
 			updateAttribute(element, "aria-pressed", state.pressed);
 			updateBooleanAttribute(element, "disabled", state.disabled);
 
-			node.addEventListener("click", handleClick);
-			node.addEventListener("keydown", handleKeydown);
+			node.addEventListener("click", handler);
+			node.addEventListener("keydown", handler);
 
 			return {
 				destroy() {
-					node.removeEventListener("click", handleClick);
-					node.removeEventListener("keydown", handleKeydown);
+					node.removeEventListener("click", handler);
+					node.removeEventListener("keydown", handler);
 					cleanup();
 				},
 			};
--- src/routes/toggle/+page.svelte
+++ src/routes/toggle/+page.svelte
@@ -1,27 +1,64 @@
-<script>
+<script lang="ts">
 	import { createPressToggle } from "$lib/builders/toggle/press.svelte";
 	import Toggle from "$lib/ui/toggle/toggle.svelte";
 
-	let checked = $state(false);
+	let disabled = $state(false);
 
 	const toggle = createPressToggle({
+		pressed: true,
+		disabled: true,
+	});
+
+	const toggle1 = createPressToggle({
 		pressed: false,
 	});
 
+	const toggle2 = createPressToggle({
+		pressed: false,
+	});
+
+	const toggle3 = createPressToggle({
+		pressed: false,
+		disabled: true,
+	});
+
 	$effect(() => {
-		toggle.state.disabled = checked;
+		toggle.state.disabled = disabled;
+		toggle1.state.disabled = disabled;
+		toggle2.state.disabled = disabled;
 	});
 </script>
 
+<h1>Toggle</h1>
+
 <label>
 	<span>disable</span>
-	<input type="checkbox" bind:checked />
+	<input type="checkbox" bind:checked={disabled} />
 </label>
 
-<button use:toggle.action> I'm {toggle.state.pressed ? "" : "not"} pressed</button>
+<h2>Button</h2>
+
+<p>
+	<button use:toggle.action> I'm {toggle.state.pressed ? "" : "not"} pressed</button>
+</p>
+
+<h2>Input</h2>
+
+<p>
+	<input
+		id="press"
+		class="outline-2 outline-red-500 checked:outline-green-500"
+		type="checkbox"
+		use:toggle1.action
+	/>
+
+	<label for="press">input toggle</label>
+</p>
+
+<h2>Styled</h2>
 
 <button
-	use:toggle.action
+	use:toggle2.action
 	aria-label="Toggle italic"
 	class="text-magnum-800 hover:bg-magnum-100 aria-pressed:bg-magnum-200 aria-pressed:text-magnum-900 grid h-9 w-9
 	place-items-center items-center justify-center rounded-md bg-white text-base
@@ -31,4 +68,4 @@
 	I
 </button>
 
-<Toggle variant="outline">toggle</Toggle>
+<Toggle builder={toggle3} variant="outline">toggle</Toggle>
