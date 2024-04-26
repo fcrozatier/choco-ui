1149166bc8b04f61565cff8c24314be4612f1905
fcrozatier <frederic.crozatier@protonmail.com>
2024-04-26 10:34:11 +0200

    add looper

---
+++ src/routes/looper/+page.svelte
@@ -0,0 +1,47 @@
+<div>
+	<button>A</button>
+
+	<label>
+		<input name="group" type="radio" checked />
+		1
+	</label>
+	<label>
+		<input name="group" type="radio" />
+		2
+	</label>
+	<label>
+		<input name="group" type="radio" />
+		3
+	</label>
+	<label>
+		<input name="group" type="radio" />
+		4
+	</label>
+
+	<button>B</button>
+</div>
+
+<div>
+	<button>A</button>
+
+	<div role="tablist">
+		<label>
+			<input role="tab" name="group2" type="radio" checked />
+			1
+		</label>
+		<label>
+			<input role="tab" name="group2" type="radio" />
+			2
+		</label>
+		<label>
+			<input role="tab" name="group2" type="radio" />
+			3
+		</label>
+		<label>
+			<input role="tab" name="group2" type="radio" />
+			4
+		</label>
+	</div>
+
+	<button>B</button>
+</div>
