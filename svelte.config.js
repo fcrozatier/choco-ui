0f611ad51519e056fcd967b6e4bea55031f2ba82
fcrozatier <frederic.crozatier@protonmail.com>
2024-04-27 18:01:14 +0200

    add target blank

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,12 @@
 import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import targetBlank from "svelte-target-blank";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess(), targetBlank({ logLevel: "warn", quietList: "/**/*.md" })],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
