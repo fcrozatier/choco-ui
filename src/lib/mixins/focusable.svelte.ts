032609155699a438ef50ce0f125820b356a61b9d
fcrozatier <frederic.crozatier@protonmail.com>
2024-04-30 16:14:22 +0200

    focus doc

--- src/lib/mixins/focusable.svelte.ts
+++ src/lib/mixins/focusable.svelte.ts
@@ -2,6 +2,13 @@ import { ChocoBase } from "../components/base.svelte";
 import type { Constructor } from "./types";
 import { manageFocus, type ManageFocusOptions } from "$lib/actions/focus/manageFocus.svelte";
 
+/**
+ * The focus mixin enhances the keyboard navigability of your components
+ *
+ * Since it relies on setting `tabindex='-1'` on some elements, the behavior is only added if js is enabled to ensure improving the experience and not degrading it.
+ *
+ * If js is not available then the elements have their default focus behavior.
+ */
 export const FocusMixin = (superclass: Constructor<ChocoBase>) => {
 	return class extends superclass {
 		#focus: ReturnType<typeof manageFocus>;
