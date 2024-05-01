56cf206079e73853ad61e67958dbf1f5984aec7d
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-01 15:53:08 +0200

    refactor with togglable mixin

--- src/lib/components/toggler.ts
+++ src/lib/components/toggler.ts
@@ -1,19 +1,4 @@
 import { ChocoBase } from "./base.svelte";
-import { TogglerMixin, type TogglerOptions } from "../mixins/togglable.svelte";
+import { Togglable } from "../mixins/togglable.svelte";
 
-export class Toggler<T extends HTMLButtonElement | HTMLInputElement> extends TogglerMixin(
-	ChocoBase<HTMLButtonElement | HTMLInputElement>,
-) {
-	constructor(options: TogglerOptions) {
-		super(options);
-	}
-
-	override action(node: T) {
-		const cleanup = super.action(node);
-		return {
-			destroy() {
-				cleanup.destroy();
-			},
-		};
-	}
-}
+export class Toggler extends Togglable(ChocoBase<HTMLButtonElement | HTMLInputElement>) {}
