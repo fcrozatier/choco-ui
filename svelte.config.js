ddab02bdfa5dcd211950c666e2b7d0e77fe1e7f9
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-01 18:20:49 +0200

    cleanup config

--- svelte.config.js
+++ svelte.config.js
@@ -4,14 +4,9 @@ import targetBlank from "svelte-target-blank";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: [vitePreprocess(), targetBlank({ logLevel: "warn", quietList: "/**/*.md" })],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 	},
 };
