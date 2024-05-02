888b09c584cacb764a7a3e5fabe14e1cb70d1dc9
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-02 18:25:43 +0200

    add focus group

--- src/lib/components/group.svelte.ts
+++ src/lib/components/group.svelte.ts
@@ -1,16 +1,38 @@
-import type { Constructor } from "$lib/mixins/types";
+import type { ManageFocusOptions } from "$lib/actions/focus/manageFocus.svelte";
+import { Focusable } from "$lib/mixins/focusable.svelte";
+import type { ChocoBase } from "./base.svelte";
 
-export class Group<Options, T extends Constructor<{ active: boolean }, Options>> {
-	#cls: T;
-	items: { active: boolean }[] = $state([]);
+export class Group<ItemOptions, Class extends ChocoBase & { active?: boolean }> {
+	#cls: new (...args: ItemOptions[]) => Class;
+
+	items: Class[] = $state([]);
 	active = $derived(this.items.filter((i) => i.active));
 
-	constructor(cls: T) {
+	constructor(cls: new (...args: ItemOptions[]) => Class) {
 		this.#cls = cls;
 	}
 
-	addItem = (options: Options) => {
+	addItem = (options: ItemOptions) => {
 		const item = new this.#cls(options);
+		this.items.push(item);
+		return item;
+	};
+}
+
+export class FocusGroup<ItemOptions, Class extends ChocoBase & { active?: boolean }> {
+	#cls: ReturnType<typeof Focusable<ItemOptions>>;
+	#options: ManageFocusOptions;
+
+	items: Class[] = $state([]);
+	active = $derived(this.items.filter((i) => i.active));
+
+	constructor(options: { cls: new (...args: ItemOptions[]) => Class; focus: ManageFocusOptions }) {
+		this.#cls = Focusable(options.cls);
+		this.#options = options.focus;
+	}
+
+	newItem = (options: ItemOptions) => {
+		const item = new this.#cls({ ...this.#options, ...options }) as Class;
 		this.items.push(item);
 		return item;
 	};
