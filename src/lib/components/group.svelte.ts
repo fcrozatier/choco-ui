c87a18e89805270b3a1563e6c1da12811d36ac28
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-03 17:55:19 +0200

    experiment with group

--- src/lib/components/group.svelte.ts
+++ src/lib/components/group.svelte.ts
@@ -4,15 +4,23 @@ import type { ChocoBase } from "./base.svelte";
 
 export class Group<ItemOptions, Class extends ChocoBase & { active?: boolean }> {
 	#cls: new (...args: ItemOptions[]) => Class;
+	#items: Class[] = [];
 
-	items: Class[] = $state([]);
+	set items(v) {
+		this.#items = v;
+	}
+
+	get items() {
+		return this.#items;
+	}
+
 	active = $derived(this.items.filter((i) => i.active));
 
 	constructor(cls: new (...args: ItemOptions[]) => Class) {
 		this.#cls = cls;
 	}
 
-	addItem = (options: ItemOptions) => {
+	createItem = (options: ItemOptions) => {
 		const item = new this.#cls(options);
 		this.items.push(item);
 		return item;
@@ -21,17 +29,17 @@ export class Group<ItemOptions, Class extends ChocoBase & { active?: boolean }> 
 
 export class FocusGroup<ItemOptions, Class extends ChocoBase & { active?: boolean }> {
 	#cls: ReturnType<typeof Focusable<ItemOptions>>;
-	#options: ManageFocusOptions;
+	#options?: ManageFocusOptions;
 
-	items: Class[] = $state([]);
+	items: Class[] = [];
 	active = $derived(this.items.filter((i) => i.active));
 
-	constructor(options: { cls: new (...args: ItemOptions[]) => Class; focus: ManageFocusOptions }) {
+	constructor(options: { cls: new (...args: ItemOptions[]) => Class; focus?: ManageFocusOptions }) {
 		this.#cls = Focusable(options.cls);
 		this.#options = options.focus;
 	}
 
-	newItem = (options: ItemOptions) => {
+	createItem = (options: ItemOptions) => {
 		const item = new this.#cls({ ...this.#options, ...options }) as Class;
 		this.items.push(item);
 		return item;
---
+++ src/routes/group/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { FocusGroup, Group } from "$lib/components/group.svelte";
+	import { ToggleButton } from "$lib/components/toggle.svelte";
+	import { nanoId } from "$lib/utils/nano";
+
+	const group = new Group(ToggleButton);
+	const focusGroup = new FocusGroup({ cls: ToggleButton });
+
+	console.log(group.items.length);
+</script>
+
+<h2>Raw groupp</h2>
+
+{#snippet toggleItem({ value, id = nanoId() })}
+	{@const item = group.createItem({ kind: "press" })}
+	<button {...item.attributes} use:item.action {id} {value} name="toggle-group">
+		{value}
+	</button>
+{/snippet}
+
+<div>
+	<legend>toggle group</legend>
+	{@render toggleItem({ value: "bold" })}
+	{@render toggleItem({ value: "underline" })}
+	{@render toggleItem({ value: "italic" })}
+</div>
+
+<h2>Focus group</h2>
+
+{#snippet focusItem({ value, id = nanoId() })}
+	{@const item = focusGroup.createItem({ kind: "press" })}
+	<button {...item.attributes} use:item.action {id} {value} name="toggle-group">
+		{value}
+	</button>
+{/snippet}
+
+<div>
+	<legend>focus group</legend>
+	{@render focusItem({ value: "bold" })}
+	{@render focusItem({ value: "underline" })}
+	{@render focusItem({ value: "italic" })}
+</div>
