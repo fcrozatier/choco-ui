7b03879eb7ee2bcecc708b65d80c0e6c82daaca4
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-07 16:50:10 +0200

    fix toggle builder signature

--- src/lib/ui/toggle-group/index.ts
+++ src/lib/ui/toggle-group/index.ts
@@ -25,7 +25,7 @@ export function setItemContext(createItem: (options: ToggleOptions) => ToggleBut
 }
 
 export function getItemContext() {
-	return getContext<(options: ToggleOptions) => ToggleButton>(toggleItemSymbol);
+	return getContext<(options?: ToggleOptions) => ToggleButton>(toggleItemSymbol);
 }
 
 const toggleVariantSymbol = Symbol();
