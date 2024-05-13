5f54528d50e37cac1fe4718c84543fc5216fe782
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-13 18:12:02 +0200

    toggle test component

---
+++ src/tests/toggle/ToggleTest.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { ToggleButton } from "$lib/components/toggle.svelte";
+
+	let disabled = $state(false);
+
+	const toggle = new ToggleButton();
+</script>
+
+<main>
+	<label>
+		<input
+			data-testid="programmatic"
+			type="checkbox"
+			onchange={() => (toggle.active = !toggle.active)}
+		/>
+		toggle programmatically
+	</label>
+
+	<button {...toggle.attributes} use:toggle.action {disabled}>press</button>
+</main>
