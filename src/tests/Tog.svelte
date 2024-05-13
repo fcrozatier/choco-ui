5d56a001a430fd06810e6b213f2500421d5345de
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-13 18:05:18 +0200

    test toggle

--- src/tests/Tog.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	let value = $state(false);
-</script>
-
-<button onclick={() => (value = !value)}>{value}</button>
---
+++ src/tests/toggle/toggle.test.ts
@@ -0,0 +1,43 @@
+import { describe, expect, it } from "vitest";
+import { render, act } from "@testing-library/svelte/svelte5";
+import { userEvent } from "@testing-library/user-event";
+import ToggleTest from "./ToggleTest.svelte";
+import { axe } from "jest-axe";
+
+describe("ToggleComponent", () => {
+	it("has no accessibility violations", async () => {
+		const { container, unmount } = render(ToggleTest);
+		expect(await axe(container)).toHaveNoViolations();
+		unmount();
+	});
+
+	it("toggles on click", async () => {
+		const { getByRole, unmount } = render(ToggleTest);
+		const button = getByRole("button");
+
+		expect(button.textContent).toBe("press");
+		expect(button.getAttribute("aria-pressed")).toBe("false");
+
+		await userEvent.click(button);
+		expect(button.getAttribute("aria-pressed")).toBe("true");
+
+		await userEvent.click(button);
+		expect(button.getAttribute("aria-pressed")).toBe("false");
+		unmount();
+	});
+
+	it("can be toggled programmatically", async () => {
+		const { getByRole, getByTestId, unmount } = render(ToggleTest);
+		const button = getByRole("button");
+		const input = getByTestId("programmatic");
+
+		expect(button.getAttribute("aria-pressed")).toBe("false");
+
+		await act(() => input.click());
+		expect(button.getAttribute("aria-pressed")).toBe("true");
+
+		await act(() => input.click());
+		expect(button.getAttribute("aria-pressed")).toBe("false");
+		unmount();
+	});
+});
