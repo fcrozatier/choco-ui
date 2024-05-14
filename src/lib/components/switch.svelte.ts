50e72a1363b07c38a6af11b4f6950e16fe5588c4
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-14 15:12:19 +0200

    add doc for switch

--- src/lib/components/switch.svelte.ts
+++ src/lib/components/switch.svelte.ts
@@ -12,6 +12,21 @@ export type SwitchOptions = {
 
 const defaults = { pressed: false, value: "" } satisfies Required<SwitchOptions>;
 
+/**
+ * ## Switch
+ *
+ * Semantics: on or off (switch toggle)
+ *
+ * The label should not change when the state changes. Use `simpleToggle` if needed.
+ *
+ * All descendants of a `switch` have role `presentation`
+ *
+ * Refs:
+ *
+ * [WAI-ARIA Switch Pattern](https://www.w3.org/WAI/ARIA/apg/patterns/switch/)
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/Switch_role#all_descendants_are_presentational
+ */
 export class Switch extends Togglable<HTMLButtonElement | HTMLInputElement>(ChocoBase) {
 	value = $derived(this.attributes?.value); // alias
 
