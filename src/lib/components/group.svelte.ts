073deca13b5212b8c6e76beb2431b201a1ea2d13
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-16 16:56:26 +0200

    refactor Group into a subclass factory

--- src/lib/components/group.svelte.ts
+++ src/lib/components/group.svelte.ts
@@ -1,5 +1,18 @@
 import { manageFocus, type ManageFocusOptions } from "$lib/actions/focus.svelte";
 import type { Action } from "svelte/action";
+import type { Togglable } from "$lib/mixins/togglable.svelte";
+
+export type GroupOptions = {
+	focus?: ManageFocusOptions;
+	/**
+	 * Whether focusing an item immediately activates it. Defaults to `false`
+	 */
+	activateOnFocus?: boolean;
+	/**
+	 * Whether only a single item can be active at a time. Defaults to `false`
+	 */
+	single?: boolean;
+};
 
 /**
  * The focus action enhances the keyboard navigability of your components
@@ -8,18 +21,66 @@ import type { Action } from "svelte/action";
  *
  * If js is not available then the elements have their default focus behavior.
  */
-export class Group<T> {
-	items: T[] = $state([]);
-	focusOptions?: ManageFocusOptions | false;
-	focusAction?: Action | undefined;
+export const Group = <T extends ReturnType<typeof Togglable>>(superclass: T) => {
+	return class {
+		#itemsMap: Map<HTMLElement, InstanceType<T>> = new Map();
+		#focusAction: Action;
 
-	constructor(focusOptions?: ManageFocusOptions | false) {
-		this.focusOptions = focusOptions;
-	}
+		options?: GroupOptions;
+		items: InstanceType<T>[] = $state([]);
+		Item: T;
 
-	initGroup() {
-		if (this.focusOptions !== false) {
-			this.focusAction = manageFocus(this.focusOptions);
+		activeItems = $derived(this.items.filter((item) => item.active));
+
+		constructor(options?: GroupOptions) {
+			this.options = options;
+
+			const map = this.#itemsMap;
+			this.#focusAction = manageFocus({
+				...options?.focus,
+				onFocus(from, to) {
+					options?.focus?.onFocus?.(from, to);
+
+					if (options?.activateOnFocus) {
+						map.get(from)?.off();
+						map.get(to)?.on();
+					}
+				},
+			});
+
+			this.Item = this.#Focusable(superclass);
 		}
-	}
-}
+
+		#Focusable = (superclass: T) => {
+			const options = this.options;
+			const items = this.items;
+			const map = this.#itemsMap;
+			const focus = this.#focusAction;
+
+			return class extends superclass {
+				constructor(...options: any[]) {
+					super(...options);
+
+					this.extendActions((node) => {
+						map.set(node, this as InstanceType<T>);
+					});
+					this.extendActions(focus);
+
+					items.push(this as InstanceType<T>);
+				}
+
+				override toggle() {
+					super.toggle();
+
+					if (this.active && options?.single) {
+						for (const other of items) {
+							if (other.active && other !== this) {
+								other.off();
+							}
+						}
+					}
+				}
+			};
+		};
+	};
+};
