57ae394fe8464abe2c5634d69528977b28ba2f30
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-20 12:23:48 +0200

    test invokable

---
+++ src/tests/invokable/InvokableTest.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { choco } from "$lib/actions/choco";
+	import { ChocoBase } from "$lib/components/base.svelte";
+	import { Invokable } from "$lib/mixins/invokable.svelte";
+	import { Togglable } from "$lib/mixins/togglable.svelte";
+
+	const Control = class extends Invokable(Togglable(ChocoBase)) {};
+
+	const control = new Control();
+	control.initInvokable({
+		control: { "aria-expanded": "false" },
+		target: { "data-open": "false" },
+		labelledBy: true,
+	});
+</script>
+
+<button use:choco={control} data-testid="invoker">button</button>
+
+<div use:choco={control.target} data-testid="target">div</div>
