66460923893965b2910bcff019b2a0e8d5401a21
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-21 15:08:18 +0200

    remove unused helpers

--- src/lib/internal/helpers.ts
+++ src/lib/internal/helpers.ts
@@ -1,45 +1,4 @@
-import type { AriaAttributes, Booleanish, HTMLAttributes } from "svelte/elements";
-
-export function updateBooleanAttribute(
-	element: HTMLElement | undefined,
-	attribute: string & NonNullable<unknown>,
-	value: Booleanish | undefined | null,
-) {
-	if (value === true || value === "true") {
-		element?.setAttribute(attribute, "");
-	} else {
-		element?.removeAttribute(attribute);
-	}
-}
-
-export function updateAttribute<T extends HTMLElement>(
-	element: T | undefined,
-	attribute: keyof AriaAttributes | keyof HTMLAttributes<T> | (string & NonNullable<unknown>),
-	value: string | number | boolean | undefined,
-) {
-	if (value !== undefined) {
-		element?.setAttribute(attribute, String(value));
-	} else {
-		element?.removeAttribute(attribute);
-	}
-}
-
-export const updateElement = (
-	node: HTMLElement | undefined,
-	state: Record<string, Booleanish | undefined> | undefined,
-) => {
-	if (!node || !state) return;
-	for (const key in state) {
-		const val = state[key];
-		if (typeof val === "string") {
-			updateAttribute(node, key, val);
-		} else if (typeof val === "boolean") {
-			updateBooleanAttribute(node, key, val);
-		} else {
-			node.removeAttribute(key);
-		}
-	}
-};
+import type { Booleanish } from "svelte/elements";
 
 /**
  * Mutates all values in place
