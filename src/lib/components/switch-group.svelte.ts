c54bf4654a4d6e79961cf8f12deb30b130557326
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-22 15:32:03 +0200

    implement switch group

---
+++ src/lib/components/switch-group.svelte.ts
@@ -0,0 +1,10 @@
+import { Group } from "./group.svelte";
+import { Switch, type SwitchOptions } from "./switch.svelte";
+
+export class SwitchGroup extends Group(Switch) {
+	active = $derived(this.activeItems.map((item) => item.value));
+
+	createItem = (options?: SwitchOptions) => {
+		return new this.Item(options);
+	};
+}
