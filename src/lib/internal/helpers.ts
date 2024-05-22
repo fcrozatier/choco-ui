c715362d907ceefeeb7735ee1844b5b2504ef34a
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-22 18:28:04 +0200

    make toggle values a pure function

--- src/lib/internal/helpers.ts
+++ src/lib/internal/helpers.ts
@@ -1,17 +1,19 @@
 import type { Booleanish } from "svelte/elements";
 
 /**
- * Mutates all values in place
+ * Pure function for toggling Booleanish values
  */
 export const toggleValues = (state: Record<string, Booleanish>) => {
+	const newState: Record<string, Booleanish> = {};
+
 	for (const key in state) {
 		const val = state[key];
 
 		if (typeof val === "boolean") {
-			state[key] = !state[key];
+			newState[key] = !val;
 		} else if (typeof val === "string") {
-			state[key] = `${val === "false"}`;
+			newState[key] = `${val === "false"}`;
 		}
 	}
-	return state;
+	return newState;
 };
