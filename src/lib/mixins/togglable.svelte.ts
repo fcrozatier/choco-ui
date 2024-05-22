210df3be9d311dd5ad1837603cf2b710f41aa1b6
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-22 18:30:30 +0200

    uncouple togglable

--- src/lib/mixins/togglable.svelte.ts
+++ src/lib/mixins/togglable.svelte.ts
@@ -4,10 +4,32 @@ import { toggleValues } from "$lib/internal/helpers";
 import type { Constructor } from "./types";
 import { addListener } from "$lib/actions/addListener";
 
+type EventName = keyof HTMLElementEventMap;
+
 export type TogglableOptions = {
-	initial: Record<string, Booleanish>;
-	active?: boolean;
+	/**
+	 * The initial state of the Togglable, describing the Booleanish attributes that are to be toggled
+	 */
+	initial?: Record<string, Booleanish>;
+	/**
+	 * Whether the initial state is the active state
+	 */
+	active: boolean;
+	/**
+	 * Event(s) on which to toggle
+	 */
+	toggle?: EventName | EventName[];
+	/**
+	 * Event(s) on which to activate
+	 */
+	on?: EventName | EventName[];
+	/**
+	 * Event(s) on which to deactivate
+	 */
+	off?: EventName | EventName[];
 };
+
+const defaults = { initial: {}, active: false } satisfies Partial<TogglableOptions>;
 
 export interface Toggler {
 	get active(): boolean;
@@ -42,41 +64,40 @@ export const Togglable = <
 		constructor(...options: any[]) {
 			super(...options);
 			this.toggle = this.toggle.bind(this);
+			this.off = this.off.bind(this);
+			this.on = this.on.bind(this);
 		}
 
-		initTogglable = (options: TogglableOptions) => {
+		initTogglable = (opts: TogglableOptions) => {
+			const options = { ...defaults, ...opts };
 			this.#attributes = options.initial;
+			this.#active = options.active;
 
-			if (options.active !== undefined) {
-				this.#active = options.active;
-			} else {
-				// If the active state is not provided try to guess
-				if (Object.values(options.initial).every((v) => v === true || v === "true")) {
-					this.#active = true;
-				} else if (Object.values(options.initial).every((v) => v === false || v === "false")) {
-					this.#active = false;
-				} else {
-					throw new Error(
-						"Could not determine the active state of the toggler. Please provide an explicit value",
-					);
-				}
+			if (options.toggle) {
+				this.extendActions(addListener(options.toggle, this.toggle));
 			}
 
-			this.extendActions(addListener("click", this.toggle));
+			if (options.on) {
+				this.extendActions(addListener(options.on, this.on));
+			}
+
+			if (options.off) {
+				this.extendActions(addListener(options.off, this.off));
+			}
 			return this;
 		};
 
 		toggle() {
 			this.#active = !this.#active;
-			toggleValues(this.#attributes);
+			this.#attributes = toggleValues(this.#attributes);
 		}
 
-		on = () => {
+		on() {
 			if (!this.active) this.toggle();
-		};
+		}
 
-		off = () => {
+		off() {
 			if (this.active) this.toggle();
-		};
+		}
 	};
 };
