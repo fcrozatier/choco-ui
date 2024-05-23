856a1e8095a1afce9c4da1aacb6f3e12a59b7eb5
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-23 17:58:31 +0200

    improve focus-visible helper

--- src/lib/components/button.svelte.ts
+++ src/lib/components/button.svelte.ts
@@ -2,10 +2,11 @@ import { addListener } from "$lib/actions/addListener";
 import { ChocoBase } from "./base.svelte";
 
 /**
- * A set of `data-*` attributes for reliably styling the `active` state, cancelling it when the cursor leaves the target.
+ * A set of `data-*` attributes for reliable styling the `active` and focus states.  `data-active` is removed when the cursor leaves the target, even if it is still pressed, to convey the "cancellability" of the action (will not trigger).
  */
 export class Button extends ChocoBase<HTMLButtonElement> {
 	#isPointerEvent = $state(false);
+	#isKeyboardEvent = $state(false);
 
 	constructor() {
 		super();
@@ -16,32 +17,48 @@ export class Button extends ChocoBase<HTMLButtonElement> {
 			"data-focus": false,
 			"data-focus-visible": false,
 		});
+
+		this.extendActions(() => {
+			const turnOn = () => (this.#isKeyboardEvent = true);
+			const turnOff = () => (this.#isKeyboardEvent = false);
+
+			document.addEventListener("keydown", turnOn);
+			document.addEventListener("keyup", turnOff);
+
+			return {
+				destroy() {
+					document.removeEventListener("keydown", turnOn);
+					document.removeEventListener("keydup", turnOff);
+				},
+			};
+		});
 		this.extendActions(
-			addListener(["pointerenter"], () => {
+			addListener("pointerenter", (e) => {
+				e.pointerType;
 				this.attributes["data-hover"] = true;
 			}),
 		);
 		this.extendActions(
-			addListener(["pointerleave"], () => {
+			addListener("pointerleave", () => {
 				this.attributes["data-hover"] = false;
 				this.attributes["data-active"] = false;
 			}),
 		);
 		this.extendActions(
-			addListener(["pointerdown"], () => {
+			addListener("pointerdown", () => {
 				this.#isPointerEvent = true;
 				this.attributes["data-active"] = true;
 			}),
 		);
 		this.extendActions(
-			addListener(["pointerup"], () => {
+			addListener("pointerup", () => {
 				this.#isPointerEvent = false;
 				this.attributes["data-active"] = false;
 			}),
 		);
 		this.extendActions(
 			addListener("focusin", () => {
-				if (!this.#isPointerEvent) {
+				if (this.#isKeyboardEvent && !this.#isPointerEvent) {
 					this.attributes["data-focus-visible"] = true;
 				}
 				this.attributes["data-focus"] = true;
