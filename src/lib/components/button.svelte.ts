edc374516c43fc43b187d5ad330f9ad604a7e4f4
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-23 16:10:31 +0200

    add focus-visible

--- src/lib/components/button.svelte.ts
+++ src/lib/components/button.svelte.ts
@@ -1,14 +1,20 @@
 import { addListener } from "$lib/actions/addListener";
 import { ChocoBase } from "./base.svelte";
 
+/**
+ * A set of `data-*` attributes for reliably styling the `active` state, cancelling it when the cursor leaves the target.
+ */
 export class Button extends ChocoBase<HTMLButtonElement> {
+	#isPointerEvent = $state(false);
+
 	constructor() {
 		super();
 
 		this.extendAttributes({
 			"data-hover": false,
-			"data-focus": false,
 			"data-active": false,
+			"data-focus": false,
+			"data-focus-visible": false,
 		});
 		this.extendActions(
 			addListener(["pointerenter"], () => {
@@ -23,21 +29,27 @@ export class Button extends ChocoBase<HTMLButtonElement> {
 		);
 		this.extendActions(
 			addListener(["pointerdown"], () => {
+				this.#isPointerEvent = true;
 				this.attributes["data-active"] = true;
 			}),
 		);
 		this.extendActions(
 			addListener(["pointerup"], () => {
+				this.#isPointerEvent = false;
 				this.attributes["data-active"] = false;
 			}),
 		);
 		this.extendActions(
 			addListener("focusin", () => {
+				if (!this.#isPointerEvent) {
+					this.attributes["data-focus-visible"] = true;
+				}
 				this.attributes["data-focus"] = true;
 			}),
 		);
 		this.extendActions(
 			addListener("focusout", () => {
+				this.attributes["data-focus-visible"] = false;
 				this.attributes["data-focus"] = false;
 			}),
 		);
