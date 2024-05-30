0c4ed874063f6b10116e3b0c2094afa29eef3704
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-30 17:58:32 +0200

    refactor cancellable as a togglable

---
+++ src/lib/components/cancellable.svelte.ts
@@ -0,0 +1,29 @@
+import { addListener } from "$lib/actions/addListener";
+import type { Togglable } from "$lib/mixins/togglable.svelte";
+
+/**
+ * ## Cancellable
+ *
+ * Adds a `data-active` attribute to normalize the `:active` state for better styling: the `data-active` attribute is removed when the cursor leaves the target (which is not the case with the CSS `:active` pseudo selector), even when it is still pressed, to convey the cancellability of the action (which will not trigger).
+ */
+export const Cancellable = <
+	U extends HTMLElement = HTMLElement,
+	T extends ReturnType<typeof Togglable<U>> = ReturnType<typeof Togglable<U>>,
+>(
+	superclass: T,
+) => {
+	return class extends superclass {
+		constructor(..._options: any[]) {
+			super();
+
+			this.initTogglable({
+				initial: { "data-active": false },
+				active: false,
+				on: "pointerdown",
+				off: "pointerup",
+			});
+
+			this.extendActions(addListener("pointerleave", this.off));
+		}
+	};
+};
