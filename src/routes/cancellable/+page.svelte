514ff5806dd438a4513338377c92abfb985c90ea
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-30 18:05:11 +0200

    cancellable example

---
+++ src/routes/cancellable/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { choco } from "$lib/actions/choco";
+	import { ChocoBase } from "$lib/components/base.svelte";
+	import { Cancellable } from "$lib/components/cancellable.svelte";
+	import { Togglable } from "$lib/mixins/togglable.svelte";
+
+	class Button extends Cancellable(Togglable(ChocoBase)) {}
+	const btn = new Button();
+</script>
+
+<p>
+	<button use:choco={btn}>Improved button</button>
+</p>
