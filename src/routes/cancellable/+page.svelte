f0d0f26ee7990b68410297c91c4d2c62fb392d95
fcrozatier <frederic.crozatier@protonmail.com>
2024-05-30 18:33:53 +0200

    move mixin around

--- src/routes/cancellable/+page.svelte
+++ src/routes/cancellable/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { choco } from "$lib/actions/choco";
 	import { ChocoBase } from "$lib/components/base.svelte";
-	import { Cancellable } from "$lib/components/cancellable.svelte";
+	import { Cancellable } from "$lib/mixins/cancellable.svelte";
 	import { Togglable } from "$lib/mixins/togglable.svelte";
 
 	class Button extends Cancellable(Togglable(ChocoBase)) {}
