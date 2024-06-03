e13f686420d95226d48b8269f3fc88c3eeadf806
fcrozatier <frederic.crozatier@protonmail.com>
2024-06-03 14:47:08 +0200

    example dialog

---
+++ src/routes/dialog/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { choco } from "$lib/actions/choco";
+	import { Dialog } from "$lib/components/dialog.svelte";
+
+	const dialog = new Dialog({ snippet: dialogSnippet, closeOnOutsideClick: true });
+</script>
+
+<button use:choco={dialog}>Flag</button>
+
+{#snippet dialogSnippet()}
+	<div class="p-8">
+		<h2>Continue?</h2>
+		<button type="submit" value="no">No</button>
+		<button type="submit" value="yes">Yes</button>
+	</div>
+{/snippet}
+
+<br />
+
+{dialog.returnValue}
