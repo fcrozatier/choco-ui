b995a5082ed15cbe8f47e4f3661f2c2e4727744f
fcrozatier <frederic.crozatier@protonmail.com>
2024-06-05 17:56:25 +0200

    typo

--- src/lib/actions/choco.ts
+++ src/lib/actions/choco.ts
@@ -2,7 +2,7 @@ import type { ChocoBase } from "$lib/components/base.svelte";
 import type { Action } from "svelte/action";
 
 export const choco = (<T extends HTMLElement>(node: NoInfer<T>, _param: ChocoBase<T>) => {
-	// Check `node` so ts does not infer a never return type
+	// Check `node` so ts does not infer a `never` return type
 	if (node) {
 		throw new Error("To remove this error add choco preprocessor to your svelte config");
 	}
