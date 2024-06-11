f1e2ed02f0ac3ec3375ec411d02d7eb554389d89
fcrozatier <frederic.crozatier@protonmail.com>
2024-06-11 16:45:38 +0200

    create vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,30 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vitest/config";
 import { svelteTesting } from "@testing-library/svelte/vite";
+import type { Plugin } from "vite";
+import { defineConfig } from "vitest/config";
+import { script } from "./src/lib/preprocessor";
+
+function svelteBinding() {
+	return {
+		name: "svelte-binding",
+		transform(code, id) {
+			if (!id.endsWith(".svelte.ts")) return { code };
+
+			const { code: after } = script({
+				content: code,
+				filename: id,
+				attributes: {},
+				markup: "",
+			});
+
+			return { code: after };
+		},
+	} satisfies Plugin;
+}
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss(), svelteTesting()],
+	plugins: [svelteBinding(), sveltekit(), tailwindcss(), svelteTesting()],
 
 	test: {
 		include: ["src/**/*.{test,spec}.{js,ts}"],
