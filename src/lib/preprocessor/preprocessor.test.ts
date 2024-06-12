6d21d107fc109709cb71d917db8727681a30c118
fcrozatier <frederic.crozatier@protonmail.com>
2024-06-12 18:50:44 +0200

    only test markup

--- src/lib/preprocessor/preprocessor.test.ts
+++ src/lib/preprocessor/preprocessor.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "vitest";
 import preprocess from "./index";
 
-const { markup, script } = preprocess();
+const { markup } = preprocess();
 
 test.each([
 	[
@@ -24,26 +24,6 @@ test.each([
 		"<input use:choco={tabList()} />",
 		"<input {...tabList().attributes} use:tabList().action />",
 	],
-])("choco preprocessor works with %s", async (_, before, after) => {
+])("choco preprocessor works with %s", (_, before, after) => {
 	expect(markup({ content: before }).code).toBe(after);
-});
-
-test.each([
-	[
-		"shorthand",
-		"bind({ active }, ['active'])",
-		"bind({ get active(){return active}, set active(v){active=v} }, ['active'])",
-	],
-	[
-		"longhand",
-		"bind({ active: activation }, ['active'])",
-		"bind({ get active(){return activation}, set active(v){activation=v} }, ['active'])",
-	],
-	[
-		"member expression",
-		"bind({ active: opts.active }, ['active'])",
-		"bind({ get active(){return opts.active}, set active(v){opts.active=v} }, ['active'])",
-	],
-])("bind expansion with %s", async (_, before, after) => {
-	expect(script({ content: before, markup: "", attributes: {} }).code).toBe(after);
 });
