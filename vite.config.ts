3cb58391344ba07a9270fe9341a68a974c42814d
fcrozatier <frederic.crozatier@protonmail.com>
2024-06-13 17:36:43 +0200

    add vite inspect

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { svelteTesting } from "@testing-library/svelte/vite";
+import Inspect from "vite-plugin-inspect";
 import { defineConfig } from "vitest/config";
 import choc from "./src/lib/plugin/plugin.js";
 
 export default defineConfig({
-	plugins: [choc(), sveltekit(), tailwindcss(), svelteTesting()],
+	plugins: [Inspect(), choc(), sveltekit(), tailwindcss(), svelteTesting()],
 
 	test: {
 		include: ["src/**/*.{test,spec}.{js,ts}"],
