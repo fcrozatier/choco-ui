75ebfc618dd3e1023753bf17a4f48ba31f49bfb1
fcrozatier <frederic.crozatier@protonmail.com>
2024-06-20 17:59:51 +0200

    format

--- plugin/macro.ts
+++ plugin/macro.ts
@@ -2,11 +2,7 @@ declare const __bindable: unique symbol;
 
 type Bindable<T> = T | (T & { [__bindable]: true });
 
-type isBindable<T> = (
-	T extends { [__bindable]: true } ? true : false
-) extends false
-	? false
-	: true;
+type isBindable<T> = (T extends { [__bindable]: true } ? true : false) extends false ? false : true;
 
 type BindableKeys<T> = keyof {
 	[K in keyof T as isBindable<T[K]> extends true ? K : never]: unknown;
@@ -20,17 +16,12 @@ export type Unbind<T extends Record<string, unknown>> = {
 	[K in keyof T]: T[K] extends Bindable<infer U> ? U : T[K];
 };
 
-export const bind = <
-	T extends Record<string, unknown>,
-	U extends BindableKeys<T>[],
->(
+export const bind = <T extends Record<string, unknown>, U extends BindableKeys<T>[]>(
 	props: NoInfer<T>,
 	_keys?: U,
 ): T | undefined => {
 	if (props) {
-		throw new Error(
-			"Add the choco plugin to your Vite config to remove this error",
-		);
+		throw new Error("Add the choco plugin to your Vite config to remove this error");
 	}
 
 	return props;
