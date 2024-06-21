02063236d87fa5b1307a120b82aa3e94814509c7
fcrozatier <frederic.crozatier@protonmail.com>
2024-06-21 15:48:25 +0200

    test switch binding

---
+++ src/tests/switch/SwitchBindTest.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { choco } from "$lib/actions/choco";
+	import { Switch } from "$lib/components/switch.svelte";
+	import { bind } from "choco-ui/plugin";
+
+	let checked = $state(false);
+
+	const toggle = new Switch(bind({ active: checked }, ["active"]));
+</script>
+
+<main>
+	<label>
+		<input data-testid="bind" type="checkbox" bind:checked />
+		toggle programmatically
+	</label>
+
+	<button data-testid="switch" use:choco={toggle}>press</button>
+</main>
--- src/tests/switch/switch.test.ts
+++ src/tests/switch/switch.test.ts
@@ -1,8 +1,9 @@
-import { describe, expect, it } from "vitest";
-import { render, act } from "@testing-library/svelte/svelte5";
+import { act, render } from "@testing-library/svelte/svelte5";
 import { userEvent } from "@testing-library/user-event";
-import SwitchTest from "./SwitchTest.svelte";
 import { axe } from "jest-axe";
+import { describe, expect, it } from "vitest";
+import SwitchBindTest from "./SwitchBindTest.svelte";
+import SwitchTest from "./SwitchTest.svelte";
 
 describe("Switch", () => {
 	it("has no accessibility violations", async () => {
@@ -38,6 +39,43 @@ describe("Switch", () => {
 
 		await act(() => input.click());
 		expect(button.getAttribute("aria-checked")).toBe("false");
+		unmount();
+	});
+});
+
+describe("Switch Bind", () => {
+	it("has no accessibility violations", async () => {
+		const { container, unmount } = render(SwitchBindTest);
+		expect(await axe(container)).toHaveNoViolations();
+		unmount();
+	});
+
+	it("switch <-> checkbox", async () => {
+		const { getByTestId, unmount } = render(SwitchBindTest);
+		const button = getByTestId("switch");
+		const checkbox = getByTestId("bind") as HTMLInputElement;
+
+		expect(button.textContent).toBe("press");
+		expect(button.getAttribute("aria-checked")).toBe("false");
+		expect(checkbox.checked).toBe(false);
+
+		// switch -> checkbox
+		await userEvent.click(button);
+		expect(button.getAttribute("aria-checked")).toBe("true");
+		expect(checkbox.checked).toBe(true);
+
+		await userEvent.click(button);
+		expect(button.getAttribute("aria-checked")).toBe("false");
+		expect(checkbox.checked).toBe(false);
+
+		// checkbox -> switch
+		await userEvent.click(checkbox);
+		expect(button.getAttribute("aria-checked")).toBe("true");
+		expect(checkbox.checked).toBe(true);
+
+		await userEvent.click(checkbox);
+		expect(button.getAttribute("aria-checked")).toBe("false");
+		expect(checkbox.checked).toBe(false);
 		unmount();
 	});
 });
