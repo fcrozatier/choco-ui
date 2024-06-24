62194cd9040cf78793d54a5e613bc4000dcbfc6b
fcrozatier <frederic.crozatier@protonmail.com>
2024-06-24 17:27:39 +0200

    fix types

--- src/lib/components/group.svelte.ts
+++ src/lib/components/group.svelte.ts
@@ -35,6 +35,7 @@ export type GroupOptions = {
 	  }
 	| {
 			exclusive?: false;
+			activateOnFocus?: never;
 	  }
 );
 
--- src/lib/components/toggle-group.svelte.ts
+++ src/lib/components/toggle-group.svelte.ts
@@ -2,9 +2,7 @@ import { Group } from "./group.svelte";
 import { ToggleButton, type ConcreteToggleOptions } from "./toggle.svelte";
 
 export class ToggleGroup extends Group(ToggleButton) {
-	active = $derived(this.activeItems.map((item) => item.value));
-
-	createItem = (options: ConcreteToggleOptions) => {
+	createItem = (options?: ConcreteToggleOptions) => {
 		return new this.Item(options);
 	};
 }
