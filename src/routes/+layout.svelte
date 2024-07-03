b889a007df5b7403d8b709bd9b5386171ea2c8cd
fcrozatier <frederic.crozatier@protonmail.com>
2024-07-03 18:15:53 +0200

    layout style

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,36 @@
 	let { children }: { children: Snippet } = $props();
 </script>
 
-<div class="flex min-h-full flex-col">
-	<div class="mx-auto max-w-prose px-8">
+<div class="grid">
+	<div class="relative">
+		<nav class="fixed w-64 p-4">
+			<h1 class="sr-only">Navigation</h1>
+			<section class="flex flex-col gap-2">
+				<h2 class="font-semibold">Guide</h2>
+				<a class="hover:bg-red-300" href="/accordion">Introduction</a>
+				<a class="hover:bg-red-300" href="/accordion">Getting Started</a>
+				<a class="hover:bg-red-300" href="/accordion">Accordion</a>
+				<a class="hover:bg-red-300" href="/accordion">Accordion</a>
+			</section>
+
+			<hr />
+
+			<section>
+				<h2 class="font-semibold">Components</h2>
+				<a href="/accordion">Accordion</a>
+			</section>
+		</nav>
+	</div>
+	<div class="max-w-prose px-8">
 		{@render children()}
 	</div>
-
-	<footer class="mt-auto">footer</footer>
 </div>
+
+<footer class="mt-auto">footer</footer>
+
+<style>
+	.grid {
+		display: grid;
+		grid-template-columns: 256px auto;
+	}
+</style>
