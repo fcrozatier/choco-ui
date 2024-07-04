619db37cf69cca42b0f1267c39c0089cf369e500
fcrozatier <frederic.crozatier@protonmail.com>
2024-07-04 19:14:32 +0200

    refactor nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,28 +1,55 @@
 <script lang="ts">
+	import { keys } from "@fcrozatier/ts-helpers";
 	import { type Snippet } from "svelte";
 	import "../app.css";
 
 	let { children }: { children: Snippet } = $props();
+
+	const paths = {
+		guides: [
+			{
+				href: "/introduction",
+				title: "Introduction",
+			},
+			{
+				href: "/getting-started",
+				title: "Getting Started",
+			},
+			{
+				href: "/preprocessor",
+				title: "Preprocessor",
+			},
+		],
+		mixins: [
+			{
+				href: "/togglable",
+				title: "Togglable",
+			},
+		],
+		components: [
+			{
+				href: "/toggle",
+				title: "Toggle",
+			},
+		],
+	};
 </script>
 
 <div class="grid">
 	<div class="relative">
-		<nav class="fixed w-64 p-4">
+		<nav class="fixed w-64 space-y-10 p-4">
 			<h1 class="sr-only">Navigation</h1>
-			<section class="flex flex-col gap-2">
-				<h2 class="font-semibold">Guide</h2>
-				<a class="hover:bg-red-300" href="/accordion">Introduction</a>
-				<a class="hover:bg-red-300" href="/accordion">Getting Started</a>
-				<a class="hover:bg-red-300" href="/accordion">Accordion</a>
-				<a class="hover:bg-red-300" href="/accordion">Accordion</a>
-			</section>
+			{#each keys(paths) as key}
+				<section class="flex flex-col">
+					<h2 class="mb-2 font-semibold capitalize">{key}</h2>
 
-			<hr />
-
-			<section>
-				<h2 class="font-semibold">Components</h2>
-				<a href="/accordion">Accordion</a>
-			</section>
+					{#each paths[key] as { href, title }}
+						<a class="text-gray hover:text-coral py-1 px-3 capitalize" href={`/${key}${href}`}
+							>{title}</a
+						>
+					{/each}
+				</section>
+			{/each}
 		</nav>
 	</div>
 	<div class="max-w-prose px-8">
