f0c4d143ec2cb0a89c1954d915d4769721fa0fb3
fcrozatier <frederic.crozatier@protonmail.com>
2024-07-05 10:08:22 +0200

    only consider svelte files

--- svelte.config.js
+++ svelte.config.js
@@ -2,17 +2,10 @@ import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import targetBlank from "svelte-target-blank";
 import choco from "./preprocessor/index.js";
-import { md } from "./src/site/preprocessor.js";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: [".svelte", ".md"],
-	preprocess: [
-		md(),
-		choco(),
-		vitePreprocess(),
-		targetBlank({ logLevel: "warn", quietList: "/**/*.md" }),
-	],
+	preprocess: [choco(), vitePreprocess(), targetBlank({ logLevel: "warn", quietList: "/**/*.md" })],
 
 	kit: {
 		adapter: adapter(),
