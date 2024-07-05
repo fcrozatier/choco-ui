a31a01979e6252241c2dab4ef182b9321b3a6369
fcrozatier <frederic.crozatier@protonmail.com>
2024-07-05 15:51:06 +0200

    improve script

--- scripts/build-docs.js
+++ scripts/build-docs.js
@@ -1,11 +1,8 @@
-import rehypeShiki from "@shikijs/rehype";
 import { mkdirSync, readdirSync, readFileSync, writeFileSync } from "node:fs";
-import { mkdir, rm } from "node:fs/promises";
+import { rm } from "node:fs/promises";
 import { dirname, join } from "node:path";
-import rehypeKatex from "rehype-katex";
 import rehypeStringify from "rehype-stringify";
 import remarkGFM from "remark-gfm";
-import remarkMath from "remark-math";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import smartyPants from "remark-smartypants";
@@ -44,49 +41,12 @@ async function parseMarkdown(content) {
 	const processor = await unified()
 		.use(remarkParse)
 		.use(remarkGFM)
-		.use(remarkMath)
 		.use(smartyPants)
 		.use(remarkRehype, { allowDangerousHtml: true })
-		.use(rehypeKatex, { output: "mathml" })
-		.use(rehypeShiki, { theme: "poimandres" })
 		.use(rehypeStringify, { allowDangerousCharacters: true, allowDangerousHtml: true })
 		.process(content);
 	return processor.toString();
 }
-
-/**
- * Replace characters with HTML entities.
- * @param {string} content
- */
-// function escapeHtml(content) {
-// 	content = content.replace(/{/g, "&#123;").replace(/}/g, "&#125;");
-
-// 	const componentRegex = /<[A-Z].*/g;
-// 	const components = content.match(componentRegex);
-// 	components?.forEach((component) => {
-// 		const replaced = component.replace("&#123;", "{").replace("&#125;", "}");
-// 		content = content.replace(component, replaced);
-// 	});
-
-// 	return content;
-// }
-
-const isMarkdownFile = /\.md$/;
-const isDocsFile = /\/docs\//;
-export const md = () => {
-	return /** @satisfies {import("svelte/compiler").PreprocessorGroup} */ {
-		name: "MD preprocessor",
-		markup: async ({ content, filename }) => {
-			if (!filename || !isMarkdownFile.test(filename) || !isDocsFile.test(filename)) {
-				return { code: content };
-			}
-
-			const { meta, markdown } = frontMatter(content);
-			const html = await parseMarkdown(markdown);
-			return { code: meta + html };
-		},
-	};
-};
 
 await rm("./docs", { recursive: true, force: true });
 
@@ -111,6 +71,7 @@ function getFiles(dir, files = []) {
 }
 
 const files = getFiles("./src/docs");
+const highlight = /<Highlighter code="(?<path>[^"]*)" *(?:lang="(?<lang>[^"]*)")? *\/>/;
 
 for (const path of files.filter((file) => file.endsWith(".md"))) {
 	if (!path.endsWith("introduction.md")) continue;
@@ -119,9 +80,27 @@ for (const path of files.filter((file) => file.endsWith(".md"))) {
 	const content = readFileSync(path, { encoding: "utf-8" });
 
 	const { meta, markdown } = frontMatter(content);
+
 	const html = await parseMarkdown(markdown);
-	const comment = `<!-- Generated file from ${path} :: Do not modify -->`;
+
+	let svelte = html;
+	let match;
+
+	while ((match = highlight.exec(svelte))) {
+		const code = readFileSync(join(dirname(path), match.groups.path), {
+			encoding: "utf-8",
+		})
+			.replace(/`/g, "\\`")
+			.replace(/\$\{/g, "\\${");
+
+		svelte = svelte.replace(
+			highlight,
+			`<Highlighter code={\`\n${code}\n\`} lang='${match.groups?.lang ?? "svelte"}'></Highlighter>`,
+		);
+	}
+
+	const comment = `<!-- Generated file from ${path} -- do not modify -->`;
 
 	mkdirSync(dirname(dest), { recursive: true });
-	writeFileSync(dest, comment + meta + html, { encoding: "utf-8" });
+	writeFileSync(dest, comment + meta + svelte, { encoding: "utf-8" });
 }
