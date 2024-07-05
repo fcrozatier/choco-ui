f13f921ccc620ce112e325f05d0d992a6b7807c2
fcrozatier <frederic.crozatier@protonmail.com>
2024-07-05 15:57:30 +0200

    format

--- scripts/build-docs.js
+++ scripts/build-docs.js
@@ -15,37 +15,37 @@ const matter = /^---(?<matter>(.|\n)*?)---/;
  * @param {string} content
  */
 function frontMatter(content) {
-  const match = content.match(matter);
-  if (!match || !match.groups?.matter) throw new Error("Missing frontmatter");
+	const match = content.match(matter);
+	if (!match || !match.groups?.matter) throw new Error("Missing frontmatter");
 
-  const meta = Object.fromEntries(
-    match.groups.matter
-      .split("\n")
-      .filter((line) => line !== "")
-      .map((line) => line.split(":").map((keyOrValue) => keyOrValue.trim())),
-  );
+	const meta = Object.fromEntries(
+		match.groups.matter
+			.split("\n")
+			.filter((line) => line !== "")
+			.map((line) => line.split(":").map((keyOrValue) => keyOrValue.trim())),
+	);
 
-  const metaString = `
+	const metaString = `
   <script context="module">
     export const meta = ${JSON.stringify(meta)}
   </script>
   `;
 
-  return { meta: metaString, markdown: content.replace(matter, "") };
+	return { meta: metaString, markdown: content.replace(matter, "") };
 }
 
 /**
  * @param {string} content
  */
 async function parseMarkdown(content) {
-  const processor = await unified()
-    .use(remarkParse)
-    .use(remarkGFM)
-    .use(smartyPants)
-    .use(remarkRehype, { allowDangerousHtml: true })
-    .use(rehypeStringify, { allowDangerousCharacters: true, allowDangerousHtml: true })
-    .process(content);
-  return processor.toString();
+	const processor = await unified()
+		.use(remarkParse)
+		.use(remarkGFM)
+		.use(smartyPants)
+		.use(remarkRehype, { allowDangerousHtml: true })
+		.use(rehypeStringify, { allowDangerousCharacters: true, allowDangerousHtml: true })
+		.process(content);
+	return processor.toString();
 }
 
 await rm("./docs", { recursive: true, force: true });
@@ -57,50 +57,50 @@ await rm("./docs", { recursive: true, force: true });
  * @returns
  */
 function getFiles(dir, files = []) {
-  let items = readdirSync(dir, { withFileTypes: true });
+	let items = readdirSync(dir, { withFileTypes: true });
 
-  for (const file of items) {
-    let path = join(dir, file.name);
-    if (file.isDirectory()) {
-      getFiles(path, files);
-    } else {
-      files.push(path);
-    }
-  }
-  return files;
+	for (const file of items) {
+		let path = join(dir, file.name);
+		if (file.isDirectory()) {
+			getFiles(path, files);
+		} else {
+			files.push(path);
+		}
+	}
+	return files;
 }
 
 const files = getFiles("./src/docs");
 const highlight = /<Highlighter code="(?<path>[^"]*)" *(?:lang="(?<lang>[^"]*)")? *\/>/;
 
 for (const path of files.filter((file) => file.endsWith(".md"))) {
-  if (!path.endsWith("introduction.md")) continue;
+	if (!path.endsWith("introduction.md")) continue;
 
-  const dest = dirname(path).replace("src", ".") + ".svelte";
-  const content = readFileSync(path, { encoding: "utf-8" });
+	const dest = dirname(path).replace("src", ".") + ".svelte";
+	const content = readFileSync(path, { encoding: "utf-8" });
 
-  const { meta, markdown } = frontMatter(content);
+	const { meta, markdown } = frontMatter(content);
 
-  const html = await parseMarkdown(markdown);
+	const html = await parseMarkdown(markdown);
 
-  let svelte = html;
-  let match;
+	let svelte = html;
+	let match;
 
-  while ((match = highlight.exec(svelte))) {
-    const code = readFileSync(join(dirname(path), match.groups.path), {
-      encoding: "utf-8",
-    })
-      .replace(/`/g, "\\`")
-      .replace(/\$\{/g, "\\${");
+	while ((match = highlight.exec(svelte))) {
+		const code = readFileSync(join(dirname(path), match.groups.path), {
+			encoding: "utf-8",
+		})
+			.replace(/`/g, "\\`")
+			.replace(/\$\{/g, "\\${");
 
-    svelte = svelte.replace(
-      highlight,
-      `<Highlighter code={\`\n${code}\n\`} lang='${match.groups?.lang ?? "svelte"}'></Highlighter>`,
-    );
-  }
+		svelte = svelte.replace(
+			highlight,
+			`<Highlighter code={\`\r\n${code}\r\n\`} lang='${match.groups?.lang ?? "svelte"}'></Highlighter>`,
+		);
+	}
 
-  const comment = `<!-- Generated file from ${path} -- do not modify -->`;
+	const comment = `<!-- Generated file from ${path} -- do not modify -->`;
 
-  mkdirSync(dirname(dest), { recursive: true });
-  writeFileSync(dest, comment + meta + svelte, { encoding: "utf-8" });
+	mkdirSync(dirname(dest), { recursive: true });
+	writeFileSync(dest, comment + meta + svelte, { encoding: "utf-8" });
 }
