2628d75e9814dfc6406a446b973a38af667386c7
fcrozatier <frederic.crozatier@protonmail.com>
2024-07-10 14:44:50 +0200

    move files

--- src/lib/components/button/button.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import { buttonVariants, type ButtonProps } from "./index.js";
-
-  let { class: className, children, variant, size, ...rest }: ButtonProps = $props();
-</script>
-
-<button class={buttonVariants({ variant, size, className })} {...rest}>
-  {@render children()}
-</button>
