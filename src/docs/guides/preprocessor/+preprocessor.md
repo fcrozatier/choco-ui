ff0d43e29b9cbff267cb0da21195e212891c9a3b
fcrozatier <frederic.crozatier@protonmail.com>
2024-07-16 17:07:54 +0200

    preprocessor guide

---
+++ src/docs/guides/preprocessor/+preprocessor.md
@@ -0,0 +1,15 @@
+---
+title: Preprocessor
+---
+
+<script lang="ts">
+  import Demo from "$components/Demo.svelte";
+</script>
+
+# Preprocessor
+
+The Choco preprocessor expands the `use:choco` shorthand syntax, which allows you to use headless Choco instances easily:
+
+<Demo file="useExample.svelte" />
+
+Under the hood it takes advantage of the fact that all classes are instances of a `ChocoBase` class with a simple contract, having attributes to spread and an action setting the behavior.
---
+++ src/docs/guides/preprocessor/useExample.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { choco } from "$lib/actions/choco.js";
+  import { ToggleButton } from "$lib/headless/toggle.svelte.js";
+
+  const toggle = new ToggleButton();
+</script>
+
+<p>
+  <!-- The shorthand syntax... -->
+  <button use:choco={toggle}> Click me. </button>
+  <span>Active: {toggle.active}</span>
+</p>
+
+<p>
+  <!-- ...is expanded using the ChocoBase contract -->
+  <button {...toggle.attributes} use:toggle.action> Click me. </button>
+  <span>Active: {toggle.active}</span>
+</p>
+
+<pre>{JSON.stringify(toggle.attributes, null, 2)}</pre>
+
+<style>
+  button {
+    cursor: pointer;
+    opacity: 60%;
+  }
+</style>
