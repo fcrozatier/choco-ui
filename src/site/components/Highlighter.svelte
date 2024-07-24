c9a6f67adb4b029aca9db0263990cb6506d6af96
fcrozatier <frederic.crozatier@protonmail.com>
2024-07-24 17:55:30 +0200

    load js

--- src/site/components/Highlighter.svelte
+++ src/site/components/Highlighter.svelte
@@ -5,12 +5,12 @@
 
   export const highlighter = await createHighlighter({
     themes: [theme],
-    langs: ["svelte", "ts", "sh"],
+    langs: ["svelte", "ts", "js", "sh"],
   });
 </script>
 
 <script lang="ts">
-  let { code, lang = "svelte" }: { code: string; lang: "svelte" | "ts" | "sh" } = $props();
+  let { code, lang = "svelte" }: { code: string; lang: "svelte" | "ts" | "js" | "sh" } = $props();
 </script>
 
 {@html highlighter.codeToHtml(code, { lang, theme })}
