8402c22de4a2c5f8b0979404fd1557cb22d805ee
fcrozatier <frederic.crozatier@protonmail.com>
2024-08-05 18:33:33 +0200

    generate map

--- preprocessor/index.js
+++ preprocessor/index.js
@@ -1,15 +1,6 @@
-import { walk as estreeWalk } from "estree-walker";
+import { walk } from "estree-walker";
 import MagicString from "magic-string";
 import { parse } from "svelte/compiler";
-
-/**
- * @param {import("svelte/compiler").Root} ast
- * @param {import(".").WalkerArgs} args
- */
-function walk(ast, args) {
-  // @ts-expect-error estree-walker doesn't want Svelte Ast type
-  return estreeWalk(ast, args);
-}
 
 export const chocoPreprocess = () => {
   return /** @satisfies {import("svelte/compiler").PreprocessorGroup} */ ({
@@ -17,25 +8,21 @@ export const chocoPreprocess = () => {
     markup: ({ content, filename }) => {
       if (!/use:choco=/.test(content)) return { code: content };
 
-      const markup = new MagicString(content, { filename });
+      const s = new MagicString(content, { filename });
       const ast = parse(content, { filename, modern: true });
 
       walk(ast, {
         enter(node) {
           if (node.type === "UseDirective" && node.name === "choco" && node.expression) {
             const expression = /** @type {import("svelte/compiler").BaseNode} */ (node.expression);
-            const stringExp = markup.slice(expression.start, expression.end);
+            const stringExp = s.slice(expression.start, expression.end);
 
-            markup.update(
-              node.start,
-              node.end,
-              `{...${stringExp}.attributes} use:${stringExp}.action`,
-            );
+            s.update(node.start, node.end, `{...${stringExp}.attributes} use:${stringExp}.action`);
           }
         },
       });
 
-      return { code: markup.toString() };
+      return { code: s.toString(), map: s.generateMap() };
     },
   });
 };
