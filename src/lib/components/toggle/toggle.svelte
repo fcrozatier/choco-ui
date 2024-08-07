46509198573bc9d80c8574040c1d024ec6435785
fcrozatier <frederic.crozatier@protonmail.com>
2024-08-07 18:26:18 +0200

    comments

--- src/lib/components/toggle/toggle.svelte
+++ src/lib/components/toggle/toggle.svelte
@@ -22,14 +22,18 @@
 <button
   use:choco={toggle}
   class={cn([
+    // Base
     "ring-offset-background hover:bg-muted hover:text-muted-foreground focus-visible:ring-ring  inline-flex items-center justify-center rounded-md text-sm font-medium text-white transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
     {
+      // Variants
       "bg-transparent": variant === "default",
       "border-input hover:bg-accent hover:text-accent-foreground border bg-transparent":
         variant === "outline",
+      // Sizes
       "h-10 px-3": size === "default",
       "h-9 px-2.5": size === "sm",
       "h-11 px-5": size === "lg",
+      // Active
       "bg-accent text-accent-foreground": toggle.active,
     },
     className,
