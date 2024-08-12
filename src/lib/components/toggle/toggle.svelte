9f0bd6d4974598a65e78811118cf64fdb70bf471
fcrozatier <frederic.crozatier@protonmail.com>
2024-08-12 17:26:00 +0200

    add cursor

--- src/lib/components/toggle/toggle.svelte
+++ src/lib/components/toggle/toggle.svelte
@@ -23,7 +23,7 @@
   use:choco={toggle}
   class={cn([
     // Base
-    "ring-offset-background hover:bg-muted hover:text-muted-foreground focus-visible:ring-ring  inline-flex items-center justify-center rounded-md text-sm font-medium text-white transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
+    "ring-offset-background hover:bg-muted hover:text-muted-foreground focus-visible:ring-ring inline-flex  cursor-pointer items-center justify-center rounded-md text-sm font-medium text-white transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
     {
       // Variants
       "bg-transparent": variant === "default",
