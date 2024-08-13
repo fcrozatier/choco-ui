efd6b50d9725e435b0fe204d64be5294c5099f46
fcrozatier <frederic.crozatier@protonmail.com>
2024-08-13 18:03:08 +0200

    better styles

--- src/routes/examples/cancellable/+page.svelte
+++ src/routes/examples/cancellable/+page.svelte
@@ -1,15 +1,28 @@
 <script lang="ts">
   import { choco } from "$lib/actions/choco.js";
   import { ChocoBase } from "$lib/headless/base.svelte";
-  import { Cancellable } from "$lib/mixins/cancellable.svelte";
+  import { Cancellable, Cancellable2, Canceller } from "$lib/mixins/cancellable.svelte";
 
   class Button extends Cancellable(ChocoBase) {}
+  class Button2 extends Cancellable2(ChocoBase) {}
   const btn = new Button();
+  const btn2 = new Button2();
+  const b = new Canceller();
 </script>
 
 <p class="p-10">
   <button class="py-2 px-4" use:choco={btn}>Improved button</button>
 </p>
+
+<p class="p-10">
+  <button class="py-2 px-4" use:choco={btn2}>Improved button 2</button>
+</p>
+
+<p class="p-10">
+  <button class="py-2 px-4" use:choco={b}>Improved button Canceller</button>
+</p>
+
+<pre>{JSON.stringify(b.attributes, null, 2)}</pre>
 
 <style>
   button {
@@ -17,11 +30,11 @@
     transition: all 200ms ease-out;
 
     &:hover {
-      background: var(--color-green-100);
+      background: var(--color-coral);
     }
 
     &[data-active="true"] {
-      background-color: var(--color-green-300);
+      background-color: var(--color-coral);
       scale: 0.95;
     }
   }
