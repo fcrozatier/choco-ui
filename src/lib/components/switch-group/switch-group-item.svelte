7d24fe8aa935bcee744660f60c78836e1dd91e5e
fcrozatier <frederic.crozatier@protonmail.com>
2024-08-14 17:28:50 +0200

    fix expression too complex

--- src/lib/components/switch-group/switch-group-item.svelte
+++ src/lib/components/switch-group/switch-group-item.svelte
@@ -6,16 +6,15 @@
   import { get } from "./index.js";
 
   let {
-    class: className,
     variant,
     active = $bindable(false),
     children,
-    ...restProps
+    value,
   }: SwitchProps & { value: string; children: Snippet } = $props();
 
   const ctxVariant = getContext("choco-variant") as ToggleProps["variant"] | undefined;
 </script>
 
-<Toggle builder={get().createItem} variant={variant ?? ctxVariant} bind:active {...restProps}>
+<Toggle builder={get().createItem} {value} variant={variant ?? ctxVariant} bind:active>
   {@render children?.()}
 </Toggle>
--- src/lib/components/switch/index.ts
+++ src/lib/components/switch/index.ts
@@ -1,12 +1,10 @@
 import type { SwitchOptions, Switch as TSwitch } from "$lib/headless/switch.svelte.js";
 import type { StripThunks } from "$lib/utils/binding.js";
-import type { HTMLInputAttributes } from "svelte/elements";
 import Switch from "./switch.svelte";
 
-export type SwitchProps = Omit<HTMLInputAttributes, "value"> &
-  StripThunks<SwitchOptions> & {
-    variant?: "default" | "outline";
-    builder?: (_options?: SwitchOptions) => TSwitch;
-  };
+export type SwitchProps = StripThunks<SwitchOptions> & {
+  variant?: "default" | "outline";
+  builder?: (_options?: SwitchOptions) => TSwitch;
+};
 
 export { Switch };
