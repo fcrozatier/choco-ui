5a57251e70501d3ae9a7ecb4a6a149e87b8a28c3
fcrozatier <frederic.crozatier@protonmail.com>
2024-08-14 12:27:29 +0200

    strip thunks

--- src/lib/components/toggle/index.ts
+++ src/lib/components/toggle/index.ts
@@ -1,12 +1,12 @@
 import type { ToggleOptions } from "$lib/headless/toggle.svelte.js";
 import type { Togglable } from "$lib/mixins/togglable.svelte.js";
+import type { StripThunks } from "$lib/utils/binding.js";
 import type { HTMLButtonAttributes } from "svelte/elements";
 import Toggle from "./toggle.svelte";
 
 export interface ToggleProps
   extends Omit<HTMLButtonAttributes, "value">,
-    Omit<ToggleOptions, "setActive" | "active"> {
-  active?: boolean;
+    StripThunks<ToggleOptions> {
   variant?: "default" | "outline";
   size?: "default" | "sm" | "lg";
   builder?: (_options?: ToggleOptions) => InstanceType<ReturnType<typeof Togglable>>;
