91858fceaae10dfad72f435a42362e06c6e6c286
fcrozatier <frederic.crozatier@protonmail.com>
2024-08-15 13:02:00 +0200

    better button types

--- src/lib/components/toggle/index.ts
+++ src/lib/components/toggle/index.ts
@@ -1,13 +1,13 @@
 import type { ToggleOptions } from "$lib/headless/toggle.svelte.js";
 import type { Togglable } from "$lib/mixins/togglable.svelte.js";
+import type { Attributes } from "$lib/mixins/types.js";
 import type { StripThunks } from "$lib/utils/binding.js";
 import type { Snippet } from "svelte";
-import type { HTMLButtonAttributes } from "svelte/elements";
 import Toggle from "./toggle.svelte";
 
 export interface ToggleProps extends StripThunks<ToggleOptions> {
   class?: string;
-  element?: Omit<HTMLButtonAttributes, "value" | "class">;
+  element?: Omit<Attributes<HTMLButtonElement>, "value" | "class">;
   variant?: "default" | "outline";
   size?: "default" | "sm" | "lg";
   children?: Snippet;
