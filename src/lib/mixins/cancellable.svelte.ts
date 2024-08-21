fca547172688c282d3d7397b4a8a4e04b9d08297
fcrozatier <frederic.crozatier@protonmail.com>
2024-08-21 18:48:21 +0200

    import mix from root

--- src/lib/mixins/cancellable.svelte.ts
+++ src/lib/mixins/cancellable.svelte.ts
@@ -1,9 +1,9 @@
 import { mergeActions } from "$lib/actions/combineActions.js";
 import { ChocoBase } from "$lib/base.svelte.js";
+import { mix } from "$lib/index.js";
 import { Togglable, ToggleBase } from "$lib/mixins/togglable.svelte.js";
 import type { Constructor, HTMLElementsMap, HTMLTag } from "$lib/utils/types.js";
 import type { Action } from "svelte/action";
-import { mix } from "./index.js";
 
 /**
  * ## Cancellable
