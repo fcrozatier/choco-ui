72d0912df14bd763541c3886e79a90ecaf3c2852
fcrozatier <frederic.crozatier@protonmail.com>
2024-08-23 16:17:49 +0200

    add menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
   import { page } from "$app/stores";
   import Icon from "$components/Icon.svelte";
+  import { clickOutside } from "$lib/actions/clickOutside.js";
+  import { ToggleButton } from "$lib/headless/toggle.svelte.js";
+  import { choco } from "$lib/index.js";
   import { keys } from "$lib/utils/index.js";
-  import { type Snippet } from "svelte";
+  import { onMount, type Snippet } from "svelte";
+  import { fly } from "svelte/transition";
   import "../site.css";
 
   let { children }: { children: Snippet } = $props();
@@ -55,37 +59,69 @@
       },
     ],
   };
+
+  const menu = new ToggleButton({ active: true });
+
+  let width = $state(0);
+
+  const resize = () => {
+    width = window.innerWidth;
+  };
+
+  onMount(() => {
+    width = window.innerWidth;
+    window.addEventListener("resize", resize);
+
+    return () => {
+      window.removeEventListener("resize", resize);
+    };
+  });
 </script>
+
+<button class="absolute top-0 left-0 md:hidden" inert={width > 48 * 16} use:choco={menu}
+  >Menu</button
+>
 
 <div class="grid">
   <div class="">
-    <nav
-      class="fixed top-0 bottom-0 flex h-full w-64 flex-col gap-10 overflow-auto p-4 pt-10 pb-8 pl-10"
-    >
-      {#each keys(paths) as key}
-        <section class="flex flex-col">
-          <h2 class=" mb-2 font-semibold capitalize">{key}</h2>
+    {#if menu.active || width > 48 * 16}
+      <nav
+        class="bg-dark-muted/95 md:bg-dark-muted/80 fixed top-0 bottom-0 flex h-full w-64 flex-col gap-10 overflow-auto p-4 pt-10 pb-8 pl-10 shadow-2xl"
+        transition:fly={{ x: -100, duration: 150 }}
+        use:clickOutside={() => {
+          console.log(width);
+          if (width < 48 * 16) {
+            menu.off();
+          }
+        }}
+      >
+        {#each keys(paths) as key}
+          <section class="flex flex-col">
+            <h2 class=" mb-2 font-semibold capitalize">{key}</h2>
 
-          {#each paths[key] as { href, title }}
-            <a
-              class="link"
-              aria-current={$page.url.pathname === `/${key}${href}` ? "page" : undefined}
-              href={`/${key}${href}`}>{title}</a
-            >
-          {/each}
-        </section>
-      {/each}
-      <footer class="mt-auto">
-        <a
-          href="https://github.com/fcrozatier/choco-ui"
-          class="fill-white opacity-50 hover:opacity-70"
-          target="_blank"
-          ><Icon name="github" width="2rem"></Icon> <span class="sr-only">GitHub</span>
-        </a>
-      </footer>
-    </nav>
+            {#each paths[key] as { href, title }}
+              <a
+                class="link"
+                aria-current={$page.url.pathname === `/${key}${href}` ? "page" : undefined}
+                href={`/${key}${href}`}>{title}</a
+              >
+            {/each}
+          </section>
+        {/each}
+        <footer class="mt-auto">
+          <a
+            href="https://github.com/fcrozatier/choco-ui"
+            class="fill-white opacity-50 hover:opacity-70"
+            target="_blank"
+          >
+            <Icon name="github" width="2rem"></Icon> <span class="sr-only">GitHub</span>
+          </a>
+        </footer>
+      </nav>
+    {/if}
   </div>
-  <main class="overflow-auto px-8 md:max-w-prose">
+
+  <main class="overflow-auto px-20 md:max-w-prose">
     {@render children()}
   </main>
 </div>
@@ -94,6 +130,11 @@
   .grid {
     display: grid;
     grid-template-columns: 16rem 1fr;
+
+    /* Since --breakpoint-md: 48rem */
+    @media screen and (max-width: 48rem) {
+      grid-template-columns: 1fr;
+    }
   }
 
   .link {
