611833621f3aed0b001b047c9086ffe62d866079
fcrozatier <frederic.crozatier@protonmail.com>
2024-09-18 09:43:13 +0200

    improve styles

--- src/lib/components/tabs/tab.svelte
+++ src/lib/components/tabs/tab.svelte
@@ -22,7 +22,7 @@
 
 <button
   class={cn(
-    "ring-offset-background focus-visible:ring-ring aria-selected:bg-background aria-selected:text-foreground inline-flex items-center justify-center whitespace-nowrap rounded-sm py-1.5 px-3 text-sm font-medium transition-all focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 aria-selected:shadow-sm",
+    "inline-flex items-center justify-center whitespace-nowrap rounded-sm py-1.5 px-3 text-sm font-medium text-gray-500 ring-offset-white transition-all hover:text-gray-700 focus-visible:ring-2 focus-visible:ring-gray-700 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 aria-selected:bg-white aria-selected:text-gray-900 aria-selected:shadow-sm",
     className,
   )}
   use:choco={tab}
