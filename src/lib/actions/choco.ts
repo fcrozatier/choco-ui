bb50861de4e968048b4b59160bd32c64fd3e2d85
fcrozatier <frederic.crozatier@protonmail.com>
2024-09-21 08:56:13 +0200

    explicitly type return

--- src/lib/actions/choco.ts
+++ src/lib/actions/choco.ts
@@ -2,11 +2,8 @@ import type { ChocoBase } from "$lib/blocks/base.svelte.js";
 import type { HTMLElementsMap, HTMLTag } from "$lib/utils/types.js";
 
 export const choco = <T extends HTMLTag>(
-  node: NoInfer<HTMLElementsMap[T]>,
+  _node: NoInfer<HTMLElementsMap[T]>,
   _param: ChocoBase<T>,
-) => {
-  // Check `node` so ts does not infer a `never` return type
-  if (node) {
-    throw new Error("To remove this error add choco preprocessor to your svelte config");
-  }
+): void => {
+  throw new Error("To remove this error add choco preprocessor to your svelte config");
 };
