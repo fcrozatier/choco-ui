4ac6bf5f88db1560c2a0d107eb45ae4d5c0016c5
fcrozatier <frederic.crozatier@protonmail.com>
2024-09-23 14:39:54 +0200

    constrain on buttons

--- src/lib/blocks/cancellable.svelte.ts
+++ src/lib/blocks/cancellable.svelte.ts
@@ -5,7 +5,7 @@ import { Toggleable } from "$lib/blocks/toggleable.svelte.js";
  *
  * Adds a `data-active` attribute to normalize the `:active` state for better styling: the `data-active` attribute is removed when the cursor leaves the target (which is not the case with the CSS `:active` pseudo selector), even when it is still pressed, to convey the cancellability of the action (which will not trigger).
  */
-export class Cancellable extends Toggleable<"a" | "button" | "input"> {
+export class Cancellable extends Toggleable<"button" | "input"> {
   constructor(options?: { active?: boolean }) {
     const active = options?.active ?? false;
     super({
