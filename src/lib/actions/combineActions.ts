8c034f051bb76d3f95f8d49f677a478a6fb69791
fcrozatier <frederic.crozatier@protonmail.com>
2024-09-23 12:32:15 +0200

    remove unknown

--- src/lib/actions/combineActions.ts
+++ src/lib/actions/combineActions.ts
@@ -3,7 +3,7 @@ import type { Action, ActionReturn } from "svelte/action";
 /**
  * Turn many parameter-less actions into one action
  */
-export function mergeActions<T extends Action<unknown>[]>(...actions: T) {
+export function mergeActions<T extends Action[]>(...actions: T) {
   return ((node) => {
     const cleanups: (void | ActionReturn)[] = [];
     for (const action of actions) {
