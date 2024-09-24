902aa09374f46a656dd544a3573f497ed63b8b76
fcrozatier <frederic.crozatier@protonmail.com>
2024-09-24 19:05:02 +0200

    use private properties

--- src/lib/blocks/cancellable.svelte.ts
+++ src/lib/blocks/cancellable.svelte.ts
@@ -8,19 +8,8 @@ import { addListener } from "chocobytes/actions/addListener.js";
  * Adds a `data-active` attribute to normalize the `:active` state for better styling: the `data-active` attribute is removed when the cursor leaves the target (which is not the case with the CSS `:active` pseudo selector), even when it is still pressed, to convey the cancellability of the action (which will not trigger).
  */
 export class Cancellable extends Toggleable<"a" | "button" | "input"> {
+  boundaries: DOMRect | undefined;
   dragging = false;
-  boundaries: DOMRect | undefined = $state();
-
-  isInside = (pointer: { x: number; y: number }) => {
-    return (
-      !!this.boundaries &&
-      pointer.x >= this.boundaries.left &&
-      pointer.x <= this.boundaries.right &&
-      pointer.y >= this.boundaries.top &&
-      pointer.y <= this.boundaries.bottom
-    );
-  };
-
   hovered = $state(false);
 
   constructor() {
@@ -46,7 +35,7 @@ export class Cancellable extends Toggleable<"a" | "button" | "input"> {
       this.boundaries = this.element.getBoundingClientRect();
       this.dragging = true;
       super.on();
-      this.element.addEventListener("pointermove", this.handlePointerMove);
+      this.element.addEventListener("pointermove", this.#handlePointerMove);
       this.element.addEventListener("click", this.#cancelClick);
     }
   };
@@ -60,9 +49,9 @@ export class Cancellable extends Toggleable<"a" | "button" | "input"> {
       this.dragging = false;
       this.element.releasePointerCapture(e.pointerId);
       this.element.removeEventListener("pointerenter", this.#on);
-      this.element.removeEventListener("pointermove", this.handlePointerMove);
+      this.element.removeEventListener("pointermove", this.#handlePointerMove);
 
-      if (this.isInside(e)) {
+      if (this.#isInside(e)) {
         this.element.removeEventListener("click", this.#cancelClick);
         this.element.click();
       }
@@ -80,17 +69,17 @@ export class Cancellable extends Toggleable<"a" | "button" | "input"> {
   #cancelClick = (e: Event) => {
     if (!(e instanceof MouseEvent)) return;
 
-    if (!this.isInside(e)) {
+    if (!this.#isInside(e)) {
       e.preventDefault();
       e.stopImmediatePropagation();
     }
   };
 
-  handlePointerMove = debounce(
+  #handlePointerMove = debounce(
     (e: Event) => {
       if (!(e instanceof PointerEvent)) return;
 
-      if (this.isInside(e)) {
+      if (this.#isInside(e)) {
         this.hovered = true;
         this.#on();
       } else {
@@ -101,4 +90,14 @@ export class Cancellable extends Toggleable<"a" | "button" | "input"> {
     50,
     true,
   );
+
+  #isInside = (pointer: { x: number; y: number }) => {
+    return (
+      !!this.boundaries &&
+      pointer.x >= this.boundaries.left &&
+      pointer.x <= this.boundaries.right &&
+      pointer.y >= this.boundaries.top &&
+      pointer.y <= this.boundaries.bottom
+    );
+  };
 }
