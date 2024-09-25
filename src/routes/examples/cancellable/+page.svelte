5f3c73e8f99bea93711c3f282fa50814c4f82e46
fcrozatier <frederic.crozatier@protonmail.com>
2024-09-25 11:53:19 +0200

    test cases

--- src/routes/examples/cancellable/+page.svelte
+++ src/routes/examples/cancellable/+page.svelte
@@ -8,22 +8,35 @@
 </script>
 
 <p class="p-10">
-  <button class="active:bg-coral py-2 px-4 hover:bg-red-400" use:choco={b1}>button</button>
   <button
-    class="active py-2 px-4 data-[hover=true]:bg-red-400"
-    data-hover={b2.hovered}
+    class="active:bg-coral border-none py-2 px-4 outline-blue-300 hover:bg-red-400 focus-visible:outline-2"
+    >button</button
+  >
+
+  <button
+    class="data-[active=true]:bg-coral py-2 px-4 outline-blue-300 data-[active=true]:scale-95 data-[focus-visible=true]:outline-2 data-[hover=true]:bg-red-400"
     onclick={() => console.log("hi")}
     use:choco={b2}>Improved button Canceller</button
   >
   <a
-    class="active inline-block py-2 px-4 data-[hover=true]:bg-red-400"
+    class="active inline-block py-2 px-4 outline-blue-300 focus-visible:outline-2 data-[hover=true]:bg-red-400"
     href="/"
-    data-hover={b3.hovered}
     use:choco={b3}>Improved a Canceller</a
   >
 </p>
 
-<pre>{JSON.stringify(b1.attributes, null, 2)}</pre>
+<form
+  onsubmit={(e) => {
+    console.log("submit");
+    e.preventDefault();
+  }}
+>
+  <button
+    class="data-[active=true]:bg-coral py-2 px-4 outline-blue-300 data-[active=true]:scale-95 data-[focus-visible=true]:outline-2 data-[hover=true]:bg-red-400"
+    use:choco={b1}>Submit</button
+  >
+</form>
+
 <pre>{JSON.stringify(b2.attributes, null, 2)}</pre>
 <pre>{JSON.stringify(b3.attributes, null, 2)}</pre>
 
@@ -31,7 +44,9 @@
   button,
   a {
     border-radius: var(--radius-sm);
-    transition: all 200ms ease-out;
+    transition:
+      background 200ms ease-out,
+      scale 200ms ease-out;
   }
 
   .active[data-active="true"] {
