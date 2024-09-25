92cdc8c447a086b192e49af0ce48d1e2a54b64a7
fcrozatier <frederic.crozatier@protonmail.com>
2024-09-25 17:58:47 +0200

    adds counters

--- src/routes/examples/cancellable/+page.svelte
+++ src/routes/examples/cancellable/+page.svelte
@@ -5,23 +5,33 @@
   const b1 = new Cancellable();
   const b2 = new Cancellable();
   const b3 = new Cancellable();
+
+  let button = $state(0);
+  let anchor = $state(0);
 </script>
 
-<p class="p-10">
+<p class="flex gap-4 p-10">
   <button
-    class="active:bg-coral border-none py-2 px-4 outline-blue-300 hover:bg-red-400 focus-visible:outline-2 active:scale-95"
+    class="active:bg-coral py-2 px-4 hover:bg-red-400 focus-visible:outline-2 focus-visible:outline-blue-300 active:scale-95"
     onclick={() => console.log("click")}>button</button
   >
 
   <button
-    class="data-[active=true]:bg-coral py-2 px-4 outline-blue-300 data-[active=true]:scale-95 data-[focus-visible=true]:outline-2 data-[hover=true]:bg-red-400"
-    onclick={() => console.log("hi")}
-    use:choco={b2}>Improved button Canceller</button
+    class="py-2 px-4"
+    onclick={() => {
+      console.log("hi");
+      button++;
+    }}
+    use:choco={b2}>Improved button Canceller <span class="tabular-nums">{button}</span></button
   >
   <a
-    class="data-[active=true]:bg-coral py-2 px-4 outline-blue-300 data-[active=true]:scale-95 data-[focus-visible=true]:outline-2 data-[hover=true]:bg-red-400"
+    class="inline-block py-2 px-4"
     href="/"
-    use:choco={b3}>Improved a Canceller</a
+    onclick={(e) => {
+      e.preventDefault();
+      anchor++;
+    }}
+    use:choco={b3}>Improved a Canceller <span class="tabular-nums">{anchor}</span></a
   >
 </p>
 
@@ -31,14 +41,13 @@
     e.preventDefault();
   }}
 >
-  <button
-    class="data-[active=true]:bg-coral py-2 px-4 outline-blue-300 data-[active=true]:scale-95 data-[focus-visible=true]:outline-2 data-[hover=true]:bg-red-400"
-    use:choco={b1}>Submit</button
-  >
+  <button class="py-2 px-4" use:choco={b1}>Submit</button>
 </form>
 
 <pre>{JSON.stringify(b2.attributes, null, 2)}</pre>
 <pre>{JSON.stringify(b3.attributes, null, 2)}</pre>
+
+<div class="grid min-h-full"><span class="mt-auto">bottom</span></div>
 
 <style>
   button,
@@ -47,5 +56,19 @@
     transition:
       background 200ms ease-out,
       scale 200ms ease-out;
+  }
+
+  /* Better control on the cascade */
+  [data-hover="true"] {
+    background-color: var(--color-red-400);
+  }
+
+  [data-active="true"] {
+    background-color: var(--color-coral);
+    scale: 95%;
+  }
+
+  [data-focus-visible="true"] {
+    outline: 2px solid var(--color-blue-300);
   }
 </style>
