aa1ee526589b8688356fc217c3d70b40d7b6462f
fcrozatier <frederic.crozatier@protonmail.com>
2024-09-26 13:05:00 +0200

    add usage example

---
+++ src/docs/blocks/cancellable/usage.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { Cancellable } from "$lib/blocks/cancellable.svelte.js";
+  import { choco } from "chocobytes/index.js";
+
+  const cancellable = new Cancellable();
+</script>
+
+<button use:choco={cancellable}>Improved</button>
